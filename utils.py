d0ec9b936e15a9badfd5344d2917436651ed4205
marcomag416 <marcomag416@gmail.com>
2025-01-23 20:23:20 +0900

    chore: cleaned unsed functions

--- utils.py
+++ utils.py
@@ -21,18 +21,3 @@ def load_img(name, split="train"):
         raise ValueError("Invalid split name")
     return Image.open(path + name)
 
-
-def load_model(model="qwen2vl"):
-    if model == "qwen2vl":
-        return Qwen2VLForConditionalGeneration.from_pretrained("Qwen/Qwen2-VL-7B-Instruct", device_map="auto")
-    else:
-        raise ValueError("Invalid model name")
-
-
-def load_processor(model="qwen2vl"):
-    if model == "qwen2vl":
-        min_pixels = 256*28*28
-        max_pixels = 512*28*28 
-        return AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
-    else:
-        raise ValueError("Invalid model name")
